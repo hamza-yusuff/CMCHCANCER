fee659921a43a8b9a3ae697a473af97831ce6151
hamza-yusuff <63330003+hamza-yusuff@users.noreply.github.com>
2020-06-04 11:31:45 +0600

    CORS

--- medical/settings.py
+++ medical/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cancer.apps.CancerConfig',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
@@ -49,13 +50,15 @@ REST_FRAMEWORK={
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST =["https://cancercmch.netlify.app"]
 ROOT_URLCONF = 'medical.urls'
 
 TEMPLATES = [
